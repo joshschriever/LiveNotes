25a29677cb86a76028b501a103c7ea322c61c583
joshschriever <joshschriever@gmail.com>
2017-01-15 18:38:52 -0600

    add permission request structure and load xml file

--- app/src/main/java/com/joshschriever/livenotes/activity/LiveNotesActivity.java
+++ app/src/main/java/com/joshschriever/livenotes/activity/LiveNotesActivity.java
@@ -1,17 +1,101 @@
 package com.joshschriever.livenotes.activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 
 import com.joshschriever.livenotes.R;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import java8.util.stream.Collectors;
+import uk.co.dolphin_com.seescoreandroid.LicenceKeyInstance;
+import uk.co.dolphin_com.seescoreandroid.SeeScoreView;
+import uk.co.dolphin_com.sscore.LoadOptions;
+import uk.co.dolphin_com.sscore.SScore;
+import uk.co.dolphin_com.sscore.ex.ScoreException;
+
+import static java8.util.stream.StreamSupport.stream;
+
 public class LiveNotesActivity extends Activity {
+
+    public static final LoadOptions loadOptions
+            = new LoadOptions(LicenceKeyInstance.SeeScoreLibKey, true);
+
+    private static final int PERMISSION_REQUEST_ALL_REQUIRED = 1;
+    private static final List<String> REQUIRED_PERMISSIONS;
+
+    static {
+        System.loadLibrary("SeeScoreLib");
+
+        REQUIRED_PERMISSIONS = new ArrayList<>();
+        REQUIRED_PERMISSIONS.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    }
+
+    private SScore score;
+
+    private SeeScoreView scoreView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_live_notes);
+
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        String[] permissionsToRequest = stream(REQUIRED_PERMISSIONS).filter(
+                s -> checkSelfPermission(s) == PackageManager.PERMISSION_DENIED)
+                                                                    .toArray(String[]::new);
+        if (permissionsToRequest.length > 0) {
+            requestPermissions(permissionsToRequest, PERMISSION_REQUEST_ALL_REQUIRED);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int code, String permissions[], int[] results) {
+        boolean allGranted = true;
+        for (int granted : results) {
+            if (granted == PackageManager.PERMISSION_DENIED) {
+                allGranted = false;
+                break;
+            }
+        }
+        if (results.length > 0 && allGranted) {
+            initialize();
+        } else {
+            checkPermissions();
+        }
+    }
+
+    private void initialize() {
+        initializeScore();
+        initializeScoreView();
+    }
+
+    private void initializeScore() {
+        File file = new File(Environment.getExternalStorageDirectory(), "BeetAnGeSample.xml");
+        try {
+            score = SScore.loadXMLFile(file, loadOptions);
+        } catch (ScoreException e) {
+            Log.e("ScoreException", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeScoreView() {
+        scoreView = (SeeScoreView) findViewById(R.id.score_view);
+        scoreView.setScore(score,
+                           stream(new ArrayList<Boolean>(score.numParts()))
+                                   .map(__ -> Boolean.TRUE).collect(Collectors.toList()),
+                           1.0f);
     }
 
 }
